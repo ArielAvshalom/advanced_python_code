339f55648835d17a20b6e79daf25b98a41bb4880
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-02-28 19:12:30 -0500

    Update grid.py
    Added explanation of grid directions

--- pokemon_project/grid.py
+++ pokemon_project/grid.py
@@ -12,7 +12,7 @@ for row_idx in range(10):
     for col_idx in range(10):
         my_grid[-1].append(0)
 
-player_position = (0,0)
+player_position = [0,0]
 
 my_grid[player_position[0]][player_position[1]] = 'X'
 
@@ -35,4 +35,46 @@ my_new_grid = road(4, 1,1,my_grid)
 # for num in range(5):
 #     my_grid[num][5] = my_road[num[0]][0]
 
+player_direction = input('which way do you want to?')
+
+if player_direction == 'w':
+    new_player_position = player_position[:]
+    new_player_position[0] = player_position[0]-1
+    
+
+    my_grid[new_player_position[0]][new_player_position[1]] = 'X'
+    my_grid[player_position[0]][player_position[1]] = 0
+
+x = [-1,1,0,0]
+y = [0,0,-1,1]
+
+x[2]
+y[2]
+
+my_test_grid = []
+for row_idx in range(3):
+    my_test_grid.append([])
+    for col_idx in range(3):
+        my_test_grid[-1].append(0)
+
+start = [1,1]
+
+my_test_grid[1][1] = 'P'
+my_test_grid[1][0] = 'W'
+my_test_grid[0][1] = 'N'
+my_test_grid[1][2] = 'E'
+my_test_grid[2][1] = 'S'
+
+my_test_grid[start[0]][start[1]+1]
+
+
+player_direction = input('which way do you want to?')
+
+if player_direction == 'n':
+    print(my_test_grid[start[0]+x[0]][start[1]+y[0]])
+if player_direction == 'w':
+    print(my_test_grid[start[0]+x[2]][start[1]+y[2]])
+
+def pass_through_grid(current_player_position):
+    pass
         
