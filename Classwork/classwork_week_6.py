9ef5b7b8b61829ca8db38f3ae86bbe83fdd0a7be
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-03-15 11:37:37 -0400

    Update classwork_week_6.py

--- Classwork/classwork_week_6.py
+++ Classwork/classwork_week_6.py
@@ -4,6 +4,8 @@ In class we created an adjacency list and adjacency matrix function which takes 
 
 In class when given an edge_pair like (1,4) we assumed that the pairing is that 1 -> 4 (ie node 1 has a directed edge to node 4). For this assignment, you're going to do the following:
 
+Part 1:
+
 Create two functions, one which will convert any number of tuple edge_pairs into an adjacency list and one which will convert them to an adjacency matrix.
 
 The difference is that now, the functions will check for an input from the user which will indicate the direction of an edge tuple. It will take in a key value pair which will be named "direction" and expected inputs are:
@@ -13,6 +15,9 @@ The difference is that now, the functions will check for an input from the user 
 
 So your functions should be able to check for and do all of these. You can either use if elif else logic for this and input separate logic for each case or use helper functions or possibly a slightly more clever approach...
 
+Once you've created these functions, you're going to do the following:
+
+Part 2:
 
 
 """
