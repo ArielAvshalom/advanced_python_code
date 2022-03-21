e32e938bc3f4c94200efd7bfa0ae0efaf7dce2a3
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-03-21 19:05:54 -0400

    Create DFS_example.py

---
+++ Week_7/DFS_example.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 18:27:16 2022
+
+
+
+psuedocode
+
+DFS(G, n):
+    visited[n] = True
+    
+    for each e that connects n:
+        if we didn't visit that edge
+        run dfs on that edge
+        
+make dict of nodes
+and set each node to be false initially
+
+for each node in graph
+    set node to be unvisited
+    
+for each node in graph
+    try to run dfs
+
+
+@author: Ariel
+"""
+
+def dfs(graph, start, visited = None):
+    if visited is None:
+        visited = set()
+        #alternatively we can use a dict as well.
+    visited.add(start)
+    
+    print(start)
+    
+    
+    for node in graph:
+        if node not in visited:
+            for nextnode in graph[start] - visited:
+                dfs(graph, nextnode, visited)
+        
+    return visited
+
+graph = {
+    '0' : set(['1','9']),
+    '1' : set(['0','8']),
+    '2' : set(['3']),
+    '3' : set(['2','4','6','7']),
+    '4' : set(['3']),
+    '5' : set(['6','7']),
+    '6' : set(['3', '5']),
+    '7' : set(['3','5','8','10','11']),
+    '8' : set(['1','7','9']),
+    '9' : set(['0','8']),
+    '10' : set(['7','11']),
+    '11' : set(['7', '10']),
+    '12' : set(['13']),
+    '13' : set(['12']),
+    '14' : set()
+    }
+
+if __name__ == "__main__":
+    answer = dfs(graph, '0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
