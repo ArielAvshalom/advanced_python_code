eb775009f82f5b3068f44e1e944b41b905149310
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-04-04 18:57:16 -0400

    Update timing_fun_python.py

--- tobe added to week 9 later/timing_fun_python.py
+++ tobe added to week 9 later/timing_fun_python.py
@@ -18,6 +18,9 @@ Threeeeeeeeeeeeeeeeeeeee ways to see the cost of your code and show the differen
 
 """
 #pip install realpython-reader
+#pip install codetiming
+
+
 
 from reader import feed
 import time
@@ -25,11 +28,29 @@ import time
 
 time.perf_counter() #this gives us the current time from a selected time in the past. That time in the past is constant.
 
+#When we get to look at exceptions and building exceptions, this could be useful
 
+class TimerError(Exception):
+    """Custom Exceptions for our Timer"""
 
-
-
-
+class Timer:
+    def __init__(self):
+        self._start_time = None
+        
+        
+    def start(self):
+        if self._start_time is not None:
+            raise TimerError(f"Our timer is running right now. Stopr it first. Use .stop() to stop it.")
+            
+        self._start_time = time.perf_counter()
+            
+    def stop(self):
+        if self._start_time is None:
+            raise TimerError(f"You can't stop something you never started. Use .start() to start the timer!")
+            
+        total_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        print(f"The elapsed time is : {total_time:0.4f} seconds")
 
 def main(article_offset):
     start_time = time.perf_counter()
@@ -42,6 +63,27 @@ def main(article_offset):
 
 if __name__ == "__main__":
     main(0)
+    
+    # t = Timer()
+    # t.start()
+    # time.sleep(1)
+    # t.stop()
+    
+    #t2 = Timer()
+    #t2.stop()
+    
+    #t3 = Timer()
+    #t3.start()
+    #t3.start()
+    
+    # t4 = Timer()
+    # t4.start()
+    # t4._start_time = 1412412
+    
+    # t4.stop()
+    
+    
+    
 
 
 
