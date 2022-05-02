3c183d6bced818d970672dd5e1a253b7a3ed61c8
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-05-02 19:48:10 -0400

    Update first_web_scraper.py

--- Week_11/first_web_scraper.py
+++ Week_11/first_web_scraper.py
@@ -17,5 +17,36 @@ end_index = html.find("</title>")
 title = html[start_index:end_index]
 
 
+#quick intro on regular ex[press]ions
+
+re.findall('ab*c', 'ac')
+#ac abc abbc abbbc
+
+re.findall('ab*.*c', 'scscsfasfqwwfasAbbbBbbdCcccdasdAc', re.IGNORECASE)
+
+
+match_results = re.search('ab*c', "ABC", re.IGNORECASE)
+
+string = "everything is <awesome> everything is cool when you work as a <team>."
+
+string1 = re.sub("<.*?>", "supercalifragilisticespionodocious", string)
+
+mytesttext = """
+html htmhl hml lalal alalla
+<TITLE > PROFILE : ARIEL IS AWESOME</title  / >
+lal ala land cake walk bored shorts
+"""
+
+pattern = "<title.*?>.*?</title.*?>"
+
+match_results2 = re.search(pattern, mytesttext, re.IGNORECASE)
+
+title = match_results2.group()
+title_without_tags = re.sub("<.*?>", "", title)
+
+
+
+
+
 
 
