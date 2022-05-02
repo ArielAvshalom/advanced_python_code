4f8a4da3c6f11e8c8fb779480efba89926d00c8c
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-05-02 19:13:06 -0400

    Update first_web_scraper.py

--- Week_11/first_web_scraper.py
+++ Week_11/first_web_scraper.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+import re
 
 url = "https://en.wikipedia.org/wiki/Omega-categorical_theory"
 
@@ -7,3 +8,14 @@ page = urlopen(url)
 html_bytes = page.read()
 
 html = html_bytes.decode("utf-8")
+
+title_index = html.find("<title>")
+
+start_index = title_index + len("<title>")
+end_index = html.find("</title>")
+
+title = html[start_index:end_index]
+
+
+
+
